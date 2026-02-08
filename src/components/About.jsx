3248e3f332d5c979ea3d9637c5d59d2be924eb38
Mansoor's <mansoors@Mansoors-MacBook-Air.local>
2026-02-08 18:34:22 -0500

    Add About section and fix navbar scroll

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
   ];
 
   return (
-    <section id="about" className="py-24 relative overflow-hidden">
+    <section id="about" className="py-24 scroll-mt-20">
       <div className="max-w-6xl mx-auto px-6">
         <div className="grid lg:grid-cols-2 gap-16 items-center">
           {/* Left Column: Narrative */}
