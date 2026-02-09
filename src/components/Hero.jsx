4aada6e2083a1b972b58912841e3c0e32d8acf97
Mansoor's <mansoors@Mansoors-MacBook-Air.local>
2026-02-08 19:21:59 -0500

    Minor Tweaks

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
           <span className="relative inline-flex rounded-full h-2 w-2 bg-emerald-500"></span>
         </span>
-        Available for Hire in Canada
+        Available for Work in Canada
       </motion.div>
 
       {/* 2. Headline */}
